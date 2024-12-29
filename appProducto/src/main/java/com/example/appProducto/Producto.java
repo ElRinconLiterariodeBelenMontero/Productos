576c69b34e947ea34517b98c99ceea2e8405ac8d
RinconLiterario <167355398+ElRinconLiterariodeBelenMontero@users.noreply.github.com>
2024-12-29 18:30:07 +0100

    new Project

---
+++ appProducto/src/main/java/com/example/appProducto/Producto.java
@@ -0,0 +1,37 @@
+package com.example.appProducto;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    private String producto;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getProducto() {
+		return producto;
+	}
+
+	public void setProducto(String producto) {
+		this.producto = producto;
+	}
+
+    
+}
